384b19f781d8e0626fbda4ff92963f062e15750e
marlepo <marcinlpotrykus@gmail.com>
2021-11-07 16:01:00 +0100

    categoryList fixes

--- views/admin/categoryList.php
+++ views/admin/categoryList.php
@@ -1,10 +1,19 @@
 <div style="padding-right: 3rem; margin-top: 3rem;">
-  <div x-data="{categories: []}"
-    x-init="fetch('<?php echo get_rest_url(null, SmallShopCategories::ROUTE_LIST); ?>').then(res => res.json()).then(res =>{categories = res;})">
+  <div x-data="getData()" x-init="fetchData()">
+
+    <div>
+      <input type="text" x-model="newCategory" id="name" name="name" placeholder="<?php echo SmallShopTranslation::translate('Category_add'); ?>">
+      <figure class="sm-shop-icon" x-on:click="addCategory()"
+        data-tooltip="<?php echo SmallShopTranslation::translate('Category_add'); ?>">
+        <?php echo SmallShopIcon::add() ?>
+      </figure>
+    </div>
 
     <table class="sm-shop-table sm-shop-table-grid-categories">
       <thead>
-        <th><?php echo SmallShopTranslation::translate('Category_name'); ?></th>
+        <th>
+          <?php echo SmallShopTranslation::translate('Category_name'); ?>
+        </th>
         <th></th>
       </thead>
       <tbody>
@@ -12,12 +21,14 @@
           <tr>
             <td x-text="category.name"></td>
             <td class="sm-shop-center">
-              <figure class="sm-shop-icon" x-on:click="edtCategory(category.id)" class="sm-shop-action" data-tooltip="<?php echo SmallShopTranslation::translate('Category_edit'); ?>">
-                <?php echo SmallShopIcon::edit(); ?>
+              <figure class="sm-shop-icon" x-on:click="edtCategory(category.id)"
+                data-tooltip="<?php echo SmallShopTranslation::translate('Category_edit'); ?>">
+                <?php echo SmallShopIcon::edit() ?>
               </figure>
 
-              <figure class="sm-shop-icon" x-on:click="delCategory(category.id)" class="sm-shop-action" data-tooltip="<?php echo SmallShopTranslation::translate('Category_delete'); ?>">
-                <?php echo SmallShopIcon::delete(); ?>
+              <figure class="sm-shop-icon" x-on:click="delCategory(category.id)"
+                data-tooltip="<?php echo SmallShopTranslation::translate('Category_delete'); ?>">
+                <?php echo SmallShopIcon::delete() ?>
               </figure>
             </td>
           </tr>
@@ -25,36 +36,56 @@
       </tbody>
     </table>
   </div>
-
-  <div x-data="{ newCat: '' }">
-    <input type="text" x-model="newCat" id="name" name="name" placeholder="<?php echo SmallShopTranslation::translate('Category_add'); ?>">
-    <figure class="sm-shop-icon" x-on:click="addCategory(newCat)" class="sm-shop-action" data-tooltip="<?php echo SmallShopTranslation::translate('Category_add'); ?>">
-      <?php echo SmallShopIcon::add(); ?>
-    </figure>
-  </div>
 </div>
 
-<script>
-  function addCategory(newCategoryName) {
-    fetch('<?php echo get_rest_url(null, SmallShopCategories::ROUTE_ADD); ?>', {
-      method: 'POST',
-      body: JSON.stringify({ name: newCategoryName })
-    })
-      .then(res => {
-        console.log(res);
-        Swal.fire('', 'You clicked the button!', 'success');
-      })
-  }
 
-  function delCategory(categoyId) {
-    fetch('<?php echo get_rest_url(null, SmallShopCategories::ROUTE_DELETE); ?>', {
-      method: 'DELETE',
-      body: JSON.stringify({ id: categoyId })
-    })
-      .then(res => { console.log(res); })
-  }
+<script type="text/javascript">
+  function getData() {
+    return {
+      searchValue: '',
+      page: 1,
+      limit: 10,
+      total: 50,
+      isLoading: false,
+      categories: null,
+      newCategory: null,
 
-  function edtCategory(categoyId) {
-    console.log('edtCategory', categoyId)
+      fetchData(page = this.page) {
+        this.page = page
+        this.isLoading = true;
+
+        fetch('<?php echo get_rest_url(null, SmallShopCategories::ROUTE_LIST); ?>')
+          .then((res) => res.json())
+          .then((data) => {
+            this.isLoading = false;
+            this.categories = data;
+          });
+      },
+      addCategory() {
+        if (!this.newCategory) {
+          Swal.fire('', 'empty', 'warning');
+          return;
+        }
+
+        fetch('<?php echo get_rest_url(null, SmallShopCategories::ROUTE_ADD); ?>', {
+          method: 'POST',
+          body: JSON.stringify({ name: this.newCategory })
+        })
+          .then(res => {
+            console.log(res);
+            Swal.fire('', 'You clicked the button!', 'success');
+          })
+      },
+      delCategory(categoyId) {
+        fetch('<?php echo get_rest_url(null, SmallShopCategories::ROUTE_DELETE); ?>', {
+          method: 'DELETE',
+          body: JSON.stringify({ id: categoyId })
+        })
+          .then(res => { console.log(res); })
+      },
+      edtCategory(categoyId) {
+        console.log('edtCategory', categoyId)
+      }
+    };
   }
 </script>
